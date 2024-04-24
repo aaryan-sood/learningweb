96d266904f23d44a2010fb28e6416fc99b34d9ee
aaryan-sood <soodaaryan7@gmail.com>
2024-04-25 01:05:56 +0530

    Added More Code

--- learningEvents/app.js
+++ learningEvents/app.js
@@ -18,3 +18,22 @@ document.querySelector('h1').onclick=() => {
     console.log('Hi what are you doing clicking a H1');
     console.log('Hey can you please stop doing that!');
 }
+
+const btn3=document.querySelector('#v3');
+btn3.addEventListener('click',()=> {console.log('Hello you clicked the third button!')})
+
+function twist()
+{
+    console.log('TWIST');
+}
+
+function shout()
+{
+    console.log('Shout');
+}
+
+const tasButton=document.querySelector('#tas');
+tasButton.onclick=twist;
+tasButton.onclick=shout;
+tasButton.addEventListener('click',twist);
+tasButton.addEventListener('click',shout);
