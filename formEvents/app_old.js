eb8b06bf80372e7c2ae95d81da19ddcba0a0e1d1
aaryan-sood <soodaaryan7@gmail.com>
2024-04-29 17:35:40 +0530

    Changes some code

--- formEvents/app_old.js
+++ formEvents/app_old.js
@@ -1,14 +1,19 @@
-const tweetform=document.querySelector('#tweetform');
+const tweetform=document.querySelector('#tweetForm');
 tweetform.addEventListener('submit',function(Event){
     Event.preventDefault();
     let username=tweetform.elements.username;
     let tweet=tweetform.elements.tweet;
-    addTweet(username,tweet);
-    // username.value='';
-    // tweet.value='';
+    addTweet(username.value,tweet.value);
+    username.value='';
+    tweet.value='';
 })
 addTweet=(username,tweet) => {
     let tweetcontainer=document.querySelector('#tweets');
     const li=document.createElement('li');
-    
+    const boldusername=document.createElement('b');
+    boldusername.append(username);
+
+    li.append(boldusername);
+    li.append(`-${tweet}`);
+    tweetcontainer.append(li);
 }
