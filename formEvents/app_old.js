efd019670ba6352fbc4a8db46f9adfad26c0cabf
aaryan-sood <soodaaryan7@gmail.com>
2024-05-31 14:17:01 +0530

    Revised some code

--- formEvents/app_old.js
+++ formEvents/app_old.js
@@ -1,19 +1,20 @@
-const tweetform=document.querySelector('#tweetForm');
-tweetform.addEventListener('submit',function(Event){
+const tweetform=document.querySelector('#Tweet-Form')
+tweetform.addEventListener('submit',function(Event)
+{
     Event.preventDefault();
-    let username=tweetform.elements.username;
-    let tweet=tweetform.elements.tweet;
+    let username=document.querySelector('#username');
+    let tweet=document.querySelector('#tweet');
     addTweet(username.value,tweet.value);
     username.value='';
-    tweet.value='';
+    tweet.value='';                                                                                                                                                                                                                                                                                  
 })
 addTweet=(username,tweet) => {
-    let tweetcontainer=document.querySelector('#tweets');
+    const ul=document.querySelector('#tweets');
     const li=document.createElement('li');
     const boldusername=document.createElement('b');
     boldusername.append(username);
-
     li.append(boldusername);
-    li.append(`-${tweet}`);
-    tweetcontainer.append(li);
+    li.append(` - ${tweet}`);
+    ul.append(li);
+
 }
