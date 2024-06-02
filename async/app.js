c2df2fa2be73b51cc9e5acb6f74d7525c84cf6d0
aaryan-sood <soodaaryan7@gmail.com>
2024-06-02 22:15:20 +0530

    added various color change methods

--- async/app.js
+++ async/app.js
@@ -1,14 +1,75 @@
-function multiply(x,y) {
-    return x * y;
+// function multiply(x,y) {
+//     return x * y;
+// }
+
+// function square(x) {
+//     return multiply(x,x);
+// }
+
+// function isRightTriangle(a,b,c){
+//     return square(a) + square(b) === square(c);
+// }
+// console.log('before');
+// isRightTriangle(3,4,5)
+// console.log('after');
+
+// console.log('I print first');
+// setTimeout(() => {
+//     console.log('I print after three seconds')
+// },3000);
+// console.log('I print second');
+
+setTimeout(()=> {
+    document.body.style.backgroundColor='red';
+    setTimeout(() => {
+        document.body.style.backgroundColor='orange'
+        setTimeout(() => {
+            document.body.style.backgroundColor='yellow'
+            setTimeout(() => {
+                document.body.style.backgroundColor='green'
+                setTimeout(() => {
+                    document.body.style.backgroundColor='blue'
+                    setTimeout(() => {
+                        document.body.style.backgroundColor='indigo'
+                        setTimeout(() => {
+                            document.body.style.backgroundColor='violet'
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
+const delayedColorChange=(newColor,delay,doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor=newColor;
+        doNext && doNext();
+    },delay)
 }
 
-function square(x) {
-    return multiply(x,x);
-}
+delayedColorChange('red',1000,() => {
+    delayedColorChange('orange',1000, ()=> {
+        delayedColorChange('yellow',1000,() => {
+            delayedColorChange('green',1000,() => {
+                delayedColorChange('blue',1000,() => {
+                    delayedColorChange('indigo',1000,()=> {
+                        delayedColorChange('violet',1000,() => {
 
-function isRightTriangle(a,b,c){
-    return square(a) + square(b) === square(c);
-}
-console.log('before');
-isRightTriangle(3,4,5)
-console.log('after');
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
+
+// searchMoviesAPI('amadeus',() => {
+//     saveToMyDB(movies,() => {
+//         // if it works run this
+//     },() => {
+//         // if it does not work run this
+//     })
+// },() => {
+//     // if API is down, or request failed
+// })
