d5b88507a19f6ad6afbd0c9504318532763d205f
aaryan-sood <soodaaryan7@gmail.com>
2024-06-04 23:55:32 +0530

    added XHTTP and fetch

---
+++ makingrequesttoanAPI/app.js
@@ -0,0 +1,50 @@
+//xml http request 
+// const req=new XMLHttpRequest();
+// req.onload=function(){
+//     console.log('It Worked!!')
+//     let data=JSON.parse(this.responseText)
+//     console.log(data.name,data.height)
+// }
+
+// req.onerror=function(){
+//     console.log('Error!!')
+//     console.log(this.response)
+// }
+// req.open("GET","https://swapi.dev/api/people/1")
+// req.send()
+
+// fetch now a second request after the first one has resolved
+// fetch("https://swapi.dev/api/people/1")
+// .then((res) => {
+//     console.log('Resolved !!!',res)
+//     return res.json()
+// })
+// .then((data) => {
+//     console.log('data',data)
+//     return fetch("https://swapi.dev/api/people/2")
+// })
+// .then((res) => {
+//     console.log("2nd request resolved!!")
+//     return res.json()
+// })
+// .then((data) => {
+//     console.log(data)
+// })
+// .catch((err) => {
+//     console.log('error !!',err);
+// })
+// Since fetch returns promises we can use async and await
+let loadStarWars=async() => {
+    try{
+        let res=await fetch("https://swapi.dev/api/people/1")
+        let data=await res.json();
+        console.log("data",data)
+        let res2=await fetch('https://swapi.dev/api/people/2');
+        let data2=await res2.json()
+        console.log('data2',data2)
+    }catch(e){
+        console.log('error !!!',e)
+    }
+    
+}
+loadStarWars()
