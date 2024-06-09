35141fec0306ae3d0cbaafd1d380d81714372361
aaryan-sood <soodaaryan7@gmail.com>
2024-06-09 16:25:53 +0530

    Added code for requiring entire directories

--- NODE/app.js
+++ NODE/app.js
@@ -1,5 +1,7 @@
-const math=require('./math');
-const {PI,square}=math;
-console.log(PI);
-console.log(square(10))
-console.log(math)
+// const math=require('./math');
+// const {PI,square}=math;
+// console.log(PI);
+// console.log(square(10))
+// console.log(math)
+const cats=require('./shelter')
+console.log('required an entire directory!!',cats)
