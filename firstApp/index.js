2857aa17eb17a5f3d71b9f7429022be3d2971c1d
aaryan-sood <soodaaryan7@gmail.com>
2024-06-11 03:50:11 +0530

    added code for path parametres string for pattern matching

--- firstApp/index.js
+++ firstApp/index.js
@@ -5,11 +5,7 @@ const port=3000;
 app.listen(port,() => {
     console.log(`listening on port ${port}`)
 })
-// app.use((req,res) => {
-//     console.log(`another request made on port  ${port}`)
-//     // res.send({red : 'color'})
-//     // res.send('<h1>This is my webpage</h1>')
-// })
+
 app.get('/cats',(req,res) => {
     res.send('this is cats page on the webpage')
 })
@@ -22,6 +18,22 @@ app.get('/',(req,res) => {
 app.post('/cats',(req,res) => {
     res.send('this is post request to /cats')
 })
+app.get('/r/:subreddit',(req,res) => {
+    // nw here we will first extract the general patter string first from req.param by destructuring it from the object and then send a response according to the string
+    let {subreddit}=req.params  // destructuring the object in JS {variable} variable here needs to have the same name as in the object
+    res.send(`<h1>requesting the ${subreddit} subreddit</h1>`)
+})
+app.get('/r/:subreddit/:comment/:postId',(req,res) => {
+    console.log(req.params)
+    let {subreddit,comment,postId}=req.params
+    res.send(`<h1>requesting the ${subreddit} subreddit and reading ${comment}'s comment having ${postId} postid</h1>`)
+})
 app.get('*',(req,res) => {
     res.send(`i don't know that path`)
 })
+
+// app.use((req,res) => {
+//     console.log(`another request made on port  ${port}`)
+//     // res.send({red : 'color'})
+//     // res.send('<h1>This is my webpage</h1>')
+// })
