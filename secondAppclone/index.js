be12b639e0bd0536cadbf2cae1498c69c842e74b
aaryan-sood <soodaaryan7@gmail.com>
2024-06-12 20:50:23 +0530

    added ejs code for conditionals and loops

--- secondAppclone/index.js
+++ secondAppclone/index.js
@@ -10,3 +10,17 @@ app.listen(port,() => {
 app.get('/',(req,res) => {
     res.render('home.ejs')
 })
+app.get('/rand',(req,res) => {
+    let num=Math.floor(Math.random() * 10) +1;
+    res.render('random.ejs',{num})
+})
+app.get('/r/:subreddit',(req,res) => {
+    let {subreddit}=req.params
+    res.render('subreddit.ejs',{subreddit});
+})
+app.get('/cats',(req,res) => {
+    let cats=[
+        'blue','Rocket','Monty','Stephanie','Winston'
+    ]
+    res.render('cats.ejs',{cats})
+})
