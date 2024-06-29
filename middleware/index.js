3e387c94deb4362ae53d1067ef01deddb9044f02
aaryan-sood <soodaaryan7@gmail.com>
2024-06-30 03:56:04 +0530

    added sasta morgan and the Date.now example from the docs

--- middleware/index.js
+++ middleware/index.js
@@ -3,8 +3,26 @@ const morgan = require('morgan');
 app=express()
 let port=3000;
 
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+
+// app.use((req,res,next) => {
+//     console.log('Hello from first middleware')
+//    return next()
+// })
+
+// app.use((req,res,next) => {
+//     console.log('Hello from second middleware')
+//    return next()
+// })
+
+app.use((req,res,next) => {
+    req.requestTime=Date.now()      //Just defined req.requestTime for every route handler  
+    console.log(req.method,req.path)
+    next()
+})
+
 app.get('/',(req,res) => {
+    console.log(`${req.requestTime}`)
     res.send('Home Page')
 })
 
