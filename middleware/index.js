9dbde45efe2265a9892c3ba03dd2c98bfc369c75
aaryan-sood <soodaaryan7@gmail.com>
2024-06-30 17:41:03 +0530

    setting up a 404 route

--- middleware/index.js
+++ middleware/index.js
@@ -14,12 +14,17 @@ let port=3000;
 //     console.log('Hello from second middleware')
 //    return next()
 // })
-
 app.use((req,res,next) => {
     req.requestTime=Date.now()      //Just defined req.requestTime for every route handler  
     console.log(req.method,req.path)
     next()
 })
+
+app.use('/dogs',(req,res,next) => {
+    console.log('I Love Dogs!!')
+    next()
+})
+
 
 app.get('/',(req,res) => {
     console.log(`${req.requestTime}`)
@@ -27,7 +32,13 @@ app.get('/',(req,res) => {
 })
 
 app.get('/dogs',(req,res) => {
+    console.log(`${req.requestTime}`) 
     res.send('Woof Woof')
+})
+
+//setting up a 404 route app.use is generally used to set up a 404 route and it is set up at the end so when nothing matches so it could render at the end 
+app.use((req,res) => {
+    res.status(404).send('Not Found!!!')
 })
 
 app.listen(port,() => {
