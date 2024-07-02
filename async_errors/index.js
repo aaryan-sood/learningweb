e2746d8ca65770d53c9ffcf0d908611431d5b1d4
aaryan-sood <soodaaryan7@gmail.com>
2024-07-03 04:39:58 +0530

    added an easy way of handling asynchronous errors

--- async_errors/index.js
+++ async_errors/index.js
@@ -19,38 +19,38 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e))
+    }
+}
+
 //view the products
-app.get('/products', async (req, res) => {
+app.get('/products', wrapAsync(async (req, res) => {
     // now in this get route we will list all our products by resonding and sending a webpage by quering the database
     //now since it takes time and returns a thenable object we add .then and .catch or make it a async function and just await it
-    try {
-        let { category } = req.query
-        if (category) {
-            const products = await Product.find({ category: category })
-            res.render('products/index.ejs', { products, category })
-        }
-        else {
-            const products = await Product.find({})
-            res.render('products/index.ejs', { products, category: 'All' })
-        }
-        console.log(category)
-    }
-    catch (e) {
-        next(e)
-    }
 
-})
-
-app.post('/products', async (req, res, next) => {
-    try {
-        const newProduct = new Product(req.body)
-        await newProduct.save()
-        res.redirect(`/products/${newProduct._id}`)
-    } catch (e) {
-        next(e)
+    let { category } = req.query
+    if (category) {
+        const products = await Product.find({ category: category })
+        res.render('products/index.ejs', { products, category })
     }
+    else {
+        const products = await Product.find({})
+        res.render('products/index.ejs', { products, category: 'All' })
+    }
+    console.log(category)
 
-})
+}))
+
+app.post('/products', wrapAsync(async (req, res, next) => {
+
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    res.redirect(`/products/${newProduct._id}`)
+
+
+}))
 
 //serve the form
 app.get('/products/new', (req, res) => {
@@ -58,64 +58,45 @@ app.get('/products/new', (req, res) => {
 })
 
 //view products in detail
-app.get('/products/:id', async (req, res, next) => {
-    try {
-        let { id } = req.params
-        let product = await Product.findById(id)
-        if (!product) {
-            throw next(new AppError('Product Not Found', 404))
-        }
-        res.render('products/show', { product })
-    } catch (e) {
-        next(e)
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
+    let { id } = req.params
+    let product = await Product.findById(id)
+    if (!product) {
+        throw next(new AppError('Product Not Found', 404))
     }
-
-})
+    res.render('products/show', { product })
+}))
 
 // to update put or patch request
-app.put('/products/:id', async (req, res, next) => {
-    try {
-        let { id } = req.params
-        let product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
-        if (product === null) {
-            throw next(new AppError('Product not found', 404))
-        }
-        else {
-            res.redirect(`/products/${product._id}`)
-        }
-    } catch (e) {
-        next(e)
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
+    let { id } = req.params
+    let product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    if (product === null) {
+        throw next(new AppError('Product not found', 404))
+    }
+    else {
+        res.redirect(`/products/${product._id}`)
     }
 
-})
+}))
 
 //edit individual products
-app.get('/products/:id/edit', async (req, res) => {
-    try{
-        let { id } = req.params
-        let product = await Product.findById(id)
-        if(!product){
-            throw next(new AppError('Product not found',404))
-        }
-        res.render('products/edit.ejs', { product, categories })
+app.get('/products/:id/edit', wrapAsync(async (req, res) => {
+    let { id } = req.params
+    let product = await Product.findById(id)
+    if (!product) {
+        throw next(new AppError('Product not found', 404))
     }
-    catch(e){
-        next(e)
-    }
-    
-})
+    res.render('products/edit.ejs', { product, categories })
 
-app.delete('/products/:id', async (req, res) => {
-    try{
-        let { id } = req.params
-        await Product.findByIdAndDelete(id)
-        res.redirect('/products')
-    }
-    catch(e){
-        next(e)
-    }
-    
-})
+
+}))
+
+app.delete('/products/:id', wrapAsync(async (req, res) => {
+    let { id } = req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect('/products')
+}))
 
 app.use((err, req, res, next) => {
     const { status = 500, message = 'Something went wrong' } = err;
