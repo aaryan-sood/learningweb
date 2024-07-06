5704b117c6b04ad5838ab7f986ae8c2a3abcc416
aaryan-sood <soodaaryan7@gmail.com>
2024-07-06 20:33:20 +0530

    added farms and Products model

---
+++ multiple_models_app/models/farms.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose')
+
+const farmSchema=new mongoose.Schema({
+    name : {
+        type : String,
+        required : [true,'Farm name is required']
+    },
+    city :{
+        type : String
+    },
+    email : {
+        type : String,
+        required : [true,'email required']
+    },
+    products : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Product'
+    }]              //Here in this farm we have an array of id's  as a farm can have many products
+}) 
