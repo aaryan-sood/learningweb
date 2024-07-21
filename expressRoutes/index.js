7025681ba7e7f7bedeb7d9c0e8d13543890ff6e6
aaryan-sood <soodaaryan7@gmail.com>
2024-07-21 16:36:37 +0530

    added another express route

--- expressRoutes/index.js
+++ expressRoutes/index.js
@@ -1,11 +1,13 @@
 const express=require('express')
 const shelterRoutes=require('./routes/shelter.js')
+const dogRoutes=require('./routes/dogs.js')
 let port=3000;
 
 app=express()
 
 //middleware to use for shelterRoutes
 app.use('/shelters',shelterRoutes)
+app.use('/dogs',dogRoutes)
 
 app.listen(port,() => {
     console.log(`app started on port ${port}`)
--- expressRoutes/routes/dogs.js
+++ expressRoutes/routes/dogs.js
@@ -1,2 +1,19 @@
 const express=require('express')
 const router=express.Router()
+
+router.get('/',(req,res) =>{
+    res.send('All Dogs')
+})
+
+router.post('/',(req,res) =>{
+    res.send('Making a dog')
+})
+
+router.get('/:id',(req,res) =>{
+    res.send('viewing a particular Dog')
+})
+
+router.get('/:id/edit',(req,res) =>{
+    res.send('editing a particular Dog')
+})
+module.exports=router
