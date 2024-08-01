b0c4c39a5efc026452736d4d3abf8ba71628c031
aaryan-sood <soodaaryan7@gmail.com>
2024-08-01 21:52:34 +0530

    completed routes module

--- expressRoutes/index.js
+++ expressRoutes/index.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const shelterRoutes=require('./routes/shelter.js')
 const dogRoutes=require('./routes/dogs.js')
+const adminRoutes=require('./routes/admin.js')
 let port=3000;
 
 app=express()
@@ -8,6 +9,7 @@ app=express()
 //middleware to use for shelterRoutes
 app.use('/shelters',shelterRoutes)
 app.use('/dogs',dogRoutes)
+app.use('/admin',adminRoutes)
 
 app.listen(port,() => {
     console.log(`app started on port ${port}`)
