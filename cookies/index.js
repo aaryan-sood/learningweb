9db0d99d56c5011f284432e21608747000b4e786
aaryan-sood <soodaaryan7@gmail.com>
2024-08-03 17:16:48 +0530

    added some comments

--- cookies/index.js
+++ cookies/index.js
@@ -1,35 +1,35 @@
-const express=require('express')
-const cookieParser=require('cookie-parser')
-let port=3000
+const express = require("express");
+const cookieParser = require("cookie-parser");
+let port = 3000;
 
-app=express()
+app = express();
 
 //middleware to parse cookies in express
-app.use(cookieParser('thisissecretcode'))  // thisissecretcode is the string used to sign the cookies
+app.use(cookieParser("thisissecretcode")); // thisissecretcode is the string used to sign the cookies
 
-app.get('/greet',(req,res)=>{
-    console.log(req.cookies)
-    res.send(`Hey there ${req.cookies.name},`)
-})
+app.get("/greet", (req, res) => {
+  console.log(req.cookies);
+  res.send(`Hey there ${req.cookies.name},`);
+});
 
-app.get('/setname',(req,res) => {
-    res.cookie('name','aaryan sood')
-    res.cookie('animal','chicken')
-    res.send('Sent you a cookie')
-})
+app.get("/setname", (req, res) => {
+  res.cookie("name", "aaryan sood");
+  res.cookie("animal", "chicken");
+  res.send("Sent you a cookie");
+});
 
-app.get('/getsignedcookie',(req,res) => {
-    res.cookie('fruit','mango',{signed : true})
-    res.send('the cookie is signed')
-})
+app.get("/getsignedcookie", (req, res) => {
+  res.cookie("fruit", "mango", { signed: true }); //to sign a cookie include a signed : true as an option
+  res.send("the cookie is signed");
+});
 
-app.get('/verifycookies',(req,res) => {
-    res.send(req.signedCookies)
-    console.log(req.cookies)
-    console.log(req.signedCookies)
-    // res.send(req.signedCookies)
-})
+app.get("/verifycookies", (req, res) => {
+  res.send(req.signedCookies);
+  console.log(req.cookies);
+  console.log(req.signedCookies); //to print signed cookies
+  // res.send(req.signedCookies)
+});
 
-app.listen(port,() => {
-    console.log(`listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
