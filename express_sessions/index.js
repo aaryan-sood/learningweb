8ef741ce5943cab5de250720cec4b23d3d6cc5b4
aaryan-sood <soodaaryan7@gmail.com>
2024-08-05 02:09:15 +0530

    added some code

--- express_sessions/index.js
+++ express_sessions/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const session = require("express-session");
 let port = 3000;
 
 app = express();
+//express middleware to set up sessions
+app.use(session({ secret: "thisisnotagoodsecret" }));
+
+app.get("/viewscount", (req, res) => {
+  if (req.session.count) {
+    req.session.count += 1;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`You have viewed this page ${req.session.count} times`);
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
